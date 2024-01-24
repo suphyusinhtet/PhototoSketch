53fbd6fe11de60e8430512b1da5939b0ef9f9c32
suphyusinhtet <53178539+suphyusinhtet@users.noreply.github.com>
2024-01-24 15:49:16 +0630

    Update phototosketchAI.py
    rename the model file

--- phototosketchAI.py
+++ phototosketchAI.py
@@ -53,8 +53,8 @@ def choosing_size(x,upload_image):
             mime="image/png",
     )
         
-small_model = tf.keras.models.load_model('AI Final Project/last_finest.h5')
-large_model = tf.keras.models.load_model('AI Final Project/imageotsketchfinetest.h5')
+small_model = tf.keras.models.load_model('AI Final Project/256size.h5')
+large_model = tf.keras.models.load_model('AI Final Project/512size.h5')
 
 st.write("Our application has some limitations: it works better on portrait face images with clear background.")
 
